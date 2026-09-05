7546f70b7612135a64c0d2dd8d50c615cbed0cdc
Aaron LaBounty <33358694+alabounty@users.noreply.github.com>
2026-09-05 10:34:20 -0400

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -2,5 +2,5 @@
 // NEVER put a Supabase secret key here.
 window.TRAINING_SYNC_CONFIG = {
   supabaseUrl: "https://wrkpszfsfrszughzjppm.supabase.co",
-  publishableKey: "sb_publishable_rzF2FNKG_jHR4ra_gYxEDw_LFt9Igx6w"
+  publishableKey: "sb_publishable_rzF2FNKG_jHR4ra_gYxEDw_LFt9Igx6"
 };
